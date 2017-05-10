1066df82a0194aa5d41556208e417a4e5e69e57d
alexr17@lakesideschool.org <alexr17@lakesideschool.org>
2017-05-10 13:49:44 -0700

    commit #2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'home/new'
+
+  get 'home/create'
+
+  get 'home/update'
+
+  get 'home/edit'
+
+  get 'home/destroy'
+
+  get 'home/index'
+
+  get 'home/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
